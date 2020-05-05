9421b655ef89393b823f176c8a8dea06afa0fddf
Culverin21 <48128893+Culverin21@users.noreply.github.com>
2020-05-05 17:33:30 +0100

    Cluedo

--- OneDrive/Documents/NetBeansProjects/LDP/src/cluedo/NewFXMain.java
+++ OneDrive/Documents/NetBeansProjects/LDP/src/cluedo/NewFXMain.java
@@ -28,6 +28,7 @@ public class NewFXMain extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
+                System.out.println("");
             }
         });
         
