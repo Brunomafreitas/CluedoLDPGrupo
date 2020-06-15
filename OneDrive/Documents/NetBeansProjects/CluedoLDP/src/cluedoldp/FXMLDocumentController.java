3488db6d60719916e823a6d21cd1f1d95c37c06d
Culverin21 <48128893+Culverin21@users.noreply.github.com>
2020-06-15 17:21:29 +0100

    10/06

--- OneDrive/Documents/NetBeansProjects/CluedoLDP/src/cluedoldp/FXMLDocumentController.java
+++ OneDrive/Documents/NetBeansProjects/CluedoLDP/src/cluedoldp/FXMLDocumentController.java
@@ -11,6 +11,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  *
@@ -20,16 +22,18 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-    
+    Image img;
+    private ImageView ivImagem;
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("Ieii");
+        System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        img = new Image( getClass().getResource("tabuleiroCluedo.png").toExternalForm() );
+        ivImagem.setImage(img);
     }    
     
 }
